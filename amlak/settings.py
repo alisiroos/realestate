58aa7c5acc58d9e5c4e451ace11b6b4e3978db51
ali siroos <your_email@example.commani.siroos@gmail.com>
2022-01-19 23:50:12 +0330

    version1

--- amlak/settings.py
+++ amlak/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2fmmo4^j+9h10&mwp5mwfodvv2n!@!nvm)&-zl0^t7h+g@pc#!'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
